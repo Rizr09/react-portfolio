e290610d0e85c003e64ef70d250bc08d82545d42
Rizr09 <rizky2004cool@gmail.com>
2022-12-31 10:59:25 +0700

    fix: added redirect to Spotify

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactMarkdown from 'react-markdown'
 
 const NowPlaying = () => {
-    const markdown = '[![spotify-github-profile](https://spotify-github-profile.vercel.app/api/view?uid=fc8y4939uhihepg1ey5bvvvs6&cover_image=true&theme=natemoo-re&show_offline=false&background_color=121212&bar_color_cover=true&bar_color=53b14f)](https://github.com/kittinan/spotify-github-profile)';
+    const markdown = '[![spotify-github-profile](https://spotify-github-profile.vercel.app/api/view?uid=fc8y4939uhihepg1ey5bvvvs6&cover_image=true&theme=natemoo-re&show_offline=false&background_color=121212&bar_color_cover=true&bar_color=53b14f)](https://spotify-github-profile.vercel.app/api/view?uid=fc8y4939uhihepg1ey5bvvvs6&redirect=true)';
 
     return (
         <div className='flex mt-5 content-center'>
